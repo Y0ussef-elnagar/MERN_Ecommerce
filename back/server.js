bdd9d86599e337c1322833e8e5a120e909913737
Youssef Elnagar <Youssef@gmail.com>
2026-08-27 11:04:02 +0300

    Prepare backend for deployment

--- back/server.js
+++ back/server.js
@@ -11,7 +11,7 @@ import notificationsRoutes from "./routes/notificationRoutes.js";
 
 const app = express();
 
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 
 app.use(express.json());
 
